9a29eaa722d41e232286f3e9a504b7c31f32b03c
Julian <julianser@hotmail.com.ar>
2023-04-10 15:00:21 -0300

    Se avanzo con el archivo administrador.js y se corrigio el required en el select de la ventana modal

--- js/administrardor.js
+++ js/administrardor.js
@@ -1,5 +1,29 @@
 import Pelicula from "./classPelicula.js";
+const btnEditar = document.getElementById('btnEditar');
+const btnAgregar = document.getElementById('btnAgregar');
+const formularioPelicula = document.getElementById('formAdministrarPelicula');
+btnEditar.addEventListener('click',crearPeli);
+btnAgregar.addEventListener('click',mostrarModalPeli);
+formularioPelicula.addEventListener('submit',cargarPelicula)
+
+const modalPelicula = new bootstrap.Modal(document.getElementById('modalAgregar'));
+
 
 // crear una nueva peli
-let nuevaPeli = new Pelicula('Super Mario','algo','url','aventura',2023,'2hs','EEUU','-');
+
+function crearPeli(){
+    let nuevaPeli = new Pelicula('Super Mario','algo','url','aventura',2023,'2hs','EEUU','-');
 console.log(nuevaPeli)
+}
+
+function mostrarModalPeli(){
+    //abrir la ventana
+    modalPelicula.show()
+    console.log('aqui vamos a crear una peli')
+}
+
+function cargarPelicula(e){
+    e.preventDefault();
+    console.log('creando la pelicula...');
+    modalPelicula.hide();
+}
